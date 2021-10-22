224fc79a599ef5b58e95fe37de6a26f1f83f723f
WallisonSantos <wallisontiagoassis206@outlook.com>
2021-10-21 22:05:07 -0300

    commit

--- src/main/java/br/com/forum/forum/controller/HelloController.java
+++ src/main/java/br/com/forum/forum/controller/HelloController.java
@@ -1,14 +1,29 @@
 package br.com.forum.forum.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.forum.forum.model.Curso;
+import br.com.forum.forum.model.Topico;
+
+@Controller
 public class HelloController {
     
     @GetMapping(value="/")
     public String getMethodName() {
         return "A Hello Word !";
+    }
+
+    @RequestMapping("/Topicos")
+    public List<Topico> lista(){
+        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+
+        return Arrays.asList(topico, topico, topico);
     }
 }
     
